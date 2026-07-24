0b30451073fe0d77c10576827cd4fb17393c080c
arupdatta88 <arup281@gmail.com>
2026-07-24 23:43:22 +0600

    delete build

--- src/app/api/engines/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-import { getEngineStatuses } from '@/lib/providers/index';
-
-export async function GET() {
-  return NextResponse.json(getEngineStatuses());
-}
